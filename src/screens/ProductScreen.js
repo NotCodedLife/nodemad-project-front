bd746ba9e9f2fa26b23602f43fe6faeb5377862c
jhon.cordoba <jhon.cordoba@smartquick.com.co>
2021-12-02 13:27:53 -0500

    cart funcionality finished

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -95,8 +95,8 @@ function ProductScreen({ match, history }) {
                                                     {
                                  
                                                         [...Array(product.countInStock).keys()].map((x) =>(
-                                                            <option key={x+1} value={x+1}>
-                                                                {x+1}
+                                                            <option key={x + 1} value={x + 1}>
+                                                                {x + 1}
                                                             </option>                  
                                                         ))
                                                     }
