c573275f76c8daa9dc2835bf9af27e5ecb178dd9
alejandro.cordoba <alejo.artia@gmail.com>
2022-10-07 16:36:39 +0200

    add : login and logout function

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,8 +4,8 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { login } from "../actions/userActions";
 import FormContainer from "../components/FormContainer";
+import { login } from "../actions/userActions";
 
 function LoginScreen({ location, history }) {
   const [email, setEmail] = useState("");
@@ -28,15 +28,18 @@ function LoginScreen({ location, history }) {
     e.preventDefault();
     dispatch(login(email, password));
   };
+
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type="email"
-            placeholder="Enter email"
+            placeholder="Enter Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
@@ -46,11 +49,12 @@ function LoginScreen({ location, history }) {
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            placeholder="Enter password "
+            placeholder="Enter Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
+
         <Button type="submit" variant="primary">
           Sign In
         </Button>
